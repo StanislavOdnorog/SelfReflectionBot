5e9c09f2c4fea2555ecc762d2fb77e8a666d6bac
Saaaas <124214595+StanislavOdnorog@users.noreply.github.com>
2023-08-22 00:41:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,20 +18,20 @@ bot = Bot(bot_api_key)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['delete'])
-async def process_start_command(message: types.Message):
+async def process_delete_command(message: types.Message):
     with open("./logs/" + str(message.from_user.id), 'w') as f:
         f.write(" ")
         await bot.send_message(message.from_user.id, "Все сообщения были успешно удалены из базы!")
 
 @dp.message_handler(commands=['download'])
-async def process_start_command(message: types.Message):
+async def process_download_command(message: types.Message):
     with open("./logs/" + str(message.from_user.id), 'rb') as f:
         path = "./logs/" + str(message.from_user.id)
         await message.answer_document(InputFile(path))
         await bot.send_message(message.from_user.id, "Лог переписки направлен!")
 
 @dp.message_handler(commands=['specialist'])
-async def process_start_command(message: types.Message):
+async def process_specialist_command(message: types.Message):
     await bot.send_message(message.from_user.id, "Обратись к специалистам сервисов yasno.live, bemeta.co, green-mirt.ru, alter.ru, специалистам других сервисов или по номеру телефона доверия: 88002000122\n\nСкачай переписку с помощью '/download' и дай специалисту с ней ознакомиться")
 
 @dp.message_handler(commands=['start'])
@@ -41,20 +41,17 @@ async def process_start_command(message: types.Message):
         await bot.send_message(message.from_user.id, start_message)
 
 @dp.message_handler()
-async def respond(message : types.Message):
-    if not message.text.startswith('/'):
-        with open("./logs/" + str(message.from_user.id), 'a') as f:
-            f.write(message.text + "\n\n")
-        with open("./logs/" + str(message.from_user.id), 'r') as f:
-            logs = ""
-            for line in (f.readlines()[-15:]):
-                logs += line
-        with open("./logs/" + str(message.from_user.id), 'a') as f:
-            ai_message = await generate_response("НЕЛЬЗЯ ПИСАТЬ ОТВЕТ БОЛЬШЕ 20-30 СЛОВ!!! Поддержи человека в любой ситуации не используя формальные термины. Будь как друг, но не отклоняйся от темы помощи (НЕЛЬЗЯ ПИСАТЬ ОТВЕТ БОЛЬШЕ 20-30 СЛОВ!!!). Ты мужчина. Помогай советуй и поддерживай, а не отправляй к специалисту в любой ситуации. Иногда можешь попросить человека продолжить мысль, если не совсем понял его. Будь проще, не заставляй человека очень нервничать. (НЕЛЬЗЯ ПИСАТЬ ОТВЕТ БОЛЬШЕ 20-30 СЛОВ!!!) Вот лог переписки и последнее сообщение, ответь ТОЛЬКО НА ПОСЛЕДНЕЕ СООБЩЕНИЕ: " + logs)
-            f.write(ai_message.choices[0].message.content + "\n\n")    
-        await bot.send_message(message.from_user.id, ai_message.choices[0].message.content)
-    else:
-        return False
+async def process_other_messages(message : types.Message):
+    with open("./logs/" + str(message.from_user.id), 'a') as f:
+        f.write(message.text + "\n\n")
+    with open("./logs/" + str(message.from_user.id), 'r') as f:
+        logs = ""
+        for line in (f.readlines()[-15:]):
+            logs += line
+    with open("./logs/" + str(message.from_user.id), 'a') as f:
+        ai_message = await generate_response("НЕЛЬЗЯ ПИСАТЬ ОТВЕТ БОЛЬШЕ 20-30 СЛОВ!!! Поддержи человека в любой ситуации не используя формальные термины. Будь как друг, но не отклоняйся от темы помощи (НЕЛЬЗЯ ПИСАТЬ ОТВЕТ БОЛЬШЕ 20-30 СЛОВ!!!). Ты мужчина. Помогай советуй и поддерживай, а не отправляй к специалисту в любой ситуации. Иногда можешь попросить человека продолжить мысль, если не совсем понял его. Будь проще, не заставляй человека очень нервничать. (НЕЛЬЗЯ ПИСАТЬ ОТВЕТ БОЛЬШЕ 20-30 СЛОВ!!!) Вот лог переписки и последнее сообщение, ответь ТОЛЬКО НА ПОСЛЕДНЕЕ СООБЩЕНИЕ: " + logs)
+        f.write(ai_message.choices[0].message.content + "\n\n")    
+    await bot.send_message(message.from_user.id, ai_message.choices[0].message.content)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates = True)
